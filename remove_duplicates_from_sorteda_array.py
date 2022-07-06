4447232c9c9b5f4e9292113d60a67dc077fd7139
jayant yadav <jayantyadav202@gmail.com>
2022-07-07 00:25:13 +0530

    buggy

--- remove_duplicates_from_sorteda_array.py
+++ remove_duplicates_from_sorteda_array.py
@@ -2,12 +2,27 @@ from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        u_idx = 0;
+        l_idx = 0;
         for i in range(len(nums)):
-            print(nums[i])
+            # BUG: the way of checking unique element is wrong.
+            # it should not compare elements
+            for j in range(u_idx,len(nums)): #start looking after the prev found unique element idx
+                if nums[i]!=nums[j]:
+                    u_idx = j
+                    print(f'{i} {u_idx}\n')
+                    break
+                else:
+                    pass
+            if i == 0:
+                pass
+            else:
+                nums[i] = nums[u_idx]
+                if u_idx == len(nums)-1:
+                    break
 
-        return 10
-
-
+        print(nums)
+        return i
 
 if __name__ == "__main__":
     nums=[0,0,1,1,1,2,2,3,3,4]
