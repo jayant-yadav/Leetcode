c3b02f32829f98c1cfd1c16cc2ac8698246e98eb
jayant yadav <jayantyadav202@gmail.com>
2022-07-07 23:38:51 +0530

    no problemo

---
+++ reverse_string.py
@@ -0,0 +1,20 @@
+from typing import  List
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+
+        for i in range(int(len(s)/2)):
+            a = s[i]
+            s[i] = s[len(s)-i-1]
+            s[len(s)-i-1] = a
+
+        print(s)
+
+
+
+
+            
+
+if __name__ == '__main__':
+    sol = Solution()
+    sol.reverseString(["h","e","l","l","o"])
