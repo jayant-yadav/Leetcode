274927d87019cd768265fe93484a5d50c0880bd3
jayant yadav <jayantyadav202@gmail.com>
2022-07-07 12:32:11 +0530

    bugcase: similar trailing nos.

--- remove_duplicates_from_sorteda_array.py
+++ remove_duplicates_from_sorteda_array.py
@@ -1,31 +1,31 @@
+# https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/727/
 from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         u_idx = 0;
-        l_idx = 0;
+        u_no = -200;
+        no_uq = 0;
         for i in range(len(nums)):
-            # BUG: the way of checking unique element is wrong.
-            # it should not compare elements
             for j in range(u_idx,len(nums)): #start looking after the prev found unique element idx
-                if nums[i]!=nums[j]:
+                if u_no!=nums[j]:
                     u_idx = j
+                    u_no = nums[j]
                     print(f'{i} {u_idx}\n')
                     break
+                elif j == len(nums)-1:
+                    no_uq = 1 
                 else:
                     pass
-            if i == 0:
-                pass
-            else:
-                nums[i] = nums[u_idx]
-                if u_idx == len(nums)-1:
-                    break
 
+            nums[i] = nums[u_idx]
+            if u_idx == len(nums)-1 or no_uq == 1:
+                break
         print(nums)
-        return i
+        return i+1
 
 if __name__ == "__main__":
-    nums=[0,0,1,1,1,2,2,3,3,4]
+    nums=[1,1,1,1,1,2,2,2]
     s = Solution()
     k = s.removeDuplicates(nums)
     print(k)
