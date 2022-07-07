4a2167370e53f676f57a947151e867f74e7f6824
jayant yadav <jayantyadav202@gmail.com>
2022-07-07 22:56:45 +0530

    alternate correct approach

--- remove_duplicates_from_sorteda_array.py
+++ remove_duplicates_from_sorteda_array.py
@@ -1,28 +1,40 @@
 # https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/727/
 from typing import List
+'''
+ Buggy Approach
+'''
+# class Solution:
+#     def removeDuplicates(self, nums: List[int]) -> int:
+#         u_idx = 0;
+#         u_no = -200;
+#         no_uq = 0;
+#         for i in range(len(nums)):
+#             for j in range(u_idx,len(nums)): #start looking after the prev found unique element idx
+#                 if u_no!=nums[j]:
+#                     u_idx = j
+#                     u_no = nums[j]
+#                     print(f'{i} {u_idx}\n')
+#                     break
+#                 elif j == len(nums)-1:
+#                     no_uq = 1 
+#                 else:
+#                     pass
 
+#             nums[i] = nums[u_idx]
+#             if u_idx == len(nums)-1 or no_uq == 1:
+#                 break
+#         print(nums)
+#         return i+1
+'''Correct Approach'''
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        u_idx = 0;
-        u_no = -200;
-        no_uq = 0;
-        for i in range(len(nums)):
-            for j in range(u_idx,len(nums)): #start looking after the prev found unique element idx
-                if u_no!=nums[j]:
-                    u_idx = j
-                    u_no = nums[j]
-                    print(f'{i} {u_idx}\n')
-                    break
-                elif j == len(nums)-1:
-                    no_uq = 1 
-                else:
-                    pass
-
-            nums[i] = nums[u_idx]
-            if u_idx == len(nums)-1 or no_uq == 1:
-                break
-        print(nums)
+        i = 0
+        for j in range(1,len(nums)):
+            if nums[i]!=nums[j]:
+                i+=1
+                nums[i]=nums[j]
         return i+1
+
 
 if __name__ == "__main__":
     nums=[1,1,1,1,1,2,2,2]
