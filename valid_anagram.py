44c75d42ba6cabd505cf4936e2868952649b72a0
jayant yadav <jayantyadav202@gmail.com>
2022-07-26 18:02:26 +0530

    sort all chars and compare

--- valid_anagram.py
+++ valid_anagram.py
@@ -1,17 +1,25 @@
 from collections import Counter
 class Solution:
+    # def isAnagram(self, s: str, t: str) -> bool:
+
+    #     d1 = Counter(s)
+    #     d2 = Counter(t)
+    #     if d1 == d2:
+    #         return True
+    #     else:
+    #         return False
+                
     def isAnagram(self, s: str, t: str) -> bool:
 
-        d1 = Counter(s)
-        d2 = Counter(t)
+        d1 = sorted(s)
+        d2 = sorted(t)
         if d1 == d2:
             return True
         else:
             return False
-                
-                
+
 if __name__ == '__main__':
     sol = Solution()
     s = "anagram"
-    t = "nagatam"
+    t = "nagaram"
     print(sol.isAnagram(s,t))
