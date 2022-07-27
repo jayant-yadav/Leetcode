b3ae52b7f1b895174286f1f35282e63fe279d70e
jayant yadav <jayantyadav202@gmail.com>
2022-07-27 23:33:55 +0530

    using no built in func

--- strstr.py
+++ strstr.py
@@ -3,13 +3,37 @@ class Solution:
 
         if len(needle) <= len(haystack):
             
-            try :
-                ans = haystack.index(needle)
-                return ans
-            except:
-                return -1 
-            
-        if len(needle) == 0:
+            # try :
+            #     ans = haystack.index(needle)
+            #     return ans
+            # except:
+            #     return -1 
+            needle_len = len(needle)
+            haystack_len = len(haystack)
+            i,j = 0,0
+            at = 0
+            while j <= (needle_len-1) and i <= (haystack_len-1):
+
+                # print(i,haystack[i])
+                print(j,needle[j])
+                if needle[j] == haystack[i] :
+                    if j == 0:
+                        at = i
+                    
+                    i+=1
+                    j+=1
+                else:
+                    j=0
+                    if at == 0:
+                        i+=1
+                    else:
+                        i = at+1
+
+            if j == needle_len:
+                return at
+
+
+        if needle_len == 0:
             return 0
 
         return -1
@@ -17,6 +41,8 @@ class Solution:
         
 if __name__ == '__main__':
     sol = Solution()
-    s = "aaaa"
-    t = "baa"
+    s = "mississippi"
+    t = "issip"
+    # s = "hello"
+    # t = 'llf'
     print(sol.strStr(s,t))
